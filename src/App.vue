89740e7e1cd46c2d234d2f839d635af877e0f4aa
jsadlercode <jasadler@gmail.com>
2025-05-29 14:10:48 +0800

    static chart added

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,17 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Button from "primevue/button";
+import LineChart from "./components/LineChart.vue";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <h1 class="text-3xl text-center mb-8 mt-4">Charts App</h1>
   </header>
 
   <main>
-    <TheWelcome />
+    <Button>A Button</Button>
+    <LineChart />
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
-import './assets/main.css'
-
+import './assets/main.css';
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+import Button from 'primevue/button';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.component('Button', Button);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }
+});
+
+app.mount('#app');
